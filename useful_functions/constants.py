0c704d6e965f767f068e82fce50843ff0b9f0847
Heikki Jylhä <hjjylh@gmail.com>
2023-05-26 22:39:06 +0300

    fixed constants

--- useful_functions/constants.py
+++ useful_functions/constants.py
@@ -5,4 +5,4 @@ daily_path = Path(__file__).parent.parent / "data" / "Daily m3 1315.csv"
 habit_path = Path(__file__).parent.parent / "data" / "Yorkshire Water consumer habits.csv"
 
 habit_cols_path = Path(__file__).parent.parent / "data" / "habits_columns.csv"
-habit_mod_path = daily_path = Path(__file__).parent.parent / "data" / "habits_column_modifiers.txt"
+habit_mod_path = Path(__file__).parent.parent / "data" / "habits_column_modifiers.txt"
